4f4107e7e71351ca48587e8b312cfd35ea09a057
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:33:03 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -29,7 +29,7 @@ EOT
     name                = string
     resource_group_name = string
     sku_name            = string
-    monitoring_enabled  = optional(bool) # Default: true
+    monitoring_enabled  = optional(bool)
     tags                = optional(map(string))
     datadog_organization = object({
       api_key           = string
